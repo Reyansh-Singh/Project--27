9e6607e6928fc51b1e899b9147282293936ffb38
Reyansh-Singh <77565352+Reyansh-Singh@users.noreply.github.com>
2021-05-14 16:32:24 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,8 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
+const Constraint = Matter.Constraint;
 var  bobObject1,bobObject2, bobObject3, bobObject4,bobObject5
 var roof,rope1,rope2,rope3,rope4,rope5
 
@@ -28,13 +27,11 @@ function setup() {
 	bobObject4 = new Bob(360,500,50)
 	bobObject5 = new Bob(380,500,50)
 
-	var bobRadius = bobObject1.r;
-
-	rope1=new  Rope(bobObject1.body,roof.body,-bobRadius*2,0)
-	rope2=new  Rope(bobObject2.body,roof.body,-bobRadius*2,0)
-	rope3=new  Rope(bobObject3.body,roof.body,-bobRadius*2,0)
-	rope4=new  Rope(bobObject4.body,roof.body,-bobRadius*2,0)
-	rope5=new  Rope(bobObject5.body,roof.body,-bobRadius*2,0)
+	rope1=new  Rope(bobObject1.body,roof.body,-80,0)
+	rope2=new  Rope(bobObject2.body,roof.body,-40,0)
+	rope3=new  Rope(bobObject3.body,roof.body,0,0)
+	rope4=new  Rope(bobObject4.body,roof.body,40,0)
+	rope5=new  Rope(bobObject5.body,roof.body,80,0)
 	
 	Engine.run(engine);
     
